590a7f85a24827a4cc8cc08f84334cd40d889f02
Shalini <shalinibvp.02@gmail.com>
2020-09-30 01:22:51 +0530

    Added Images for increase decrease and delete items

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,31 @@
 import React from 'react';
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price:9000,
+            title:'Dog',
+            qty:1
+        }
+    }
     render(){
+        const {price,title,qty}=this.state;
         return(
             <div className='cart-item'>
                 <div className='left-block'>
                     <img style={styles.image} />
                 </div>
                 <div className='right-block'>
-                    <div style={{fontSize:'xx-large'}}>Phone</div>
-                    <div style={{color:'red'}}>Rs 900</div>
-                    <div style={{fontSize:'xx-small'}}>Quantity: 1</div>
-                    <div cart-item-actions> 
+                    <div style={{fontSize:'xx-large'}}>{this.state.title}</div>
+                    <div style={{color:'red'}}>{price}</div>
+                    <div style={{fontSize:'xx-small'}}>Qty:{qty}</div>
+                    <div className= 'cart-item-actions'> 
                         {/* Buttons */}
+                        <img alt='increase' className='action-icons' src='https://www.flaticon.com/svg/static/icons/svg/753/753317.svg' ></img>
+                        <img alt='decrease' className='action-icons' src='https://www.flaticon.com/svg/static/icons/svg/929/929430.svg'></img>
+                        <img src="https://www.flaticon.com/svg/static/icons/svg/3221/3221897.svg" alt="delete" className='action-icons'/>
+                        
+
                     </div>
                 </div>
             </div>
