d307ab50f2f672a8c98f8dad23dfeaa3dd5390c0
Shalini <shalinibvp.02@gmail.com>
2020-11-24 07:36:23 +0530

    Integrated firebase firestore db and added respective functionalities

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from 'firebase/app'
 
 class  App extends React.Component{
  
@@ -10,116 +11,104 @@ class  App extends React.Component{
 
       this.state = {
 
-        product:[
-            {
-                title:"Fridge",
-                price:20000,
-                qty:1,
-                img:'https://www.flaticon.com/svg/static/icons/svg/1041/1041363.svg',
-                id:'1'
-            },
-            {
-                title:"Watch",
-                price:1000,
-                qty:1,
-                img:'https://www.flaticon.com/svg/static/icons/svg/2972/2972531.svg',
-                id:'2'
-            },
-            {
-                title:"Washing Machine IFB",
-                price:40000,
-                qty:1,
-                img:'https://www.flaticon.com/premium-icon/icons/svg/2283/2283894.svg',
-                id:'3'
-            },
-            {
-                
-                title:"MacBook",
-                price:100000,
-                qty:1,
-                img:'https://www.flaticon.com/svg/static/icons/svg/644/644461.svg',
-                id:'4'
-            },
-            {
-                
-                title:"IPhone12",
-                price:800000,
-                qty:1,
-                img:'https://www.flaticon.com/svg/static/icons/svg/644/644458.svg',
-                id:'5'
-            },
-            {
-                
-                title:"Sofa",
-                price:800000,
-                qty:1,
-                img:'https://www.flaticon.com/svg/static/icons/svg/3773/3773825.svg',
-                id:'6'
-            },
-            {
-                
-                title:"Dinning Table",
-                price:800000,
-                qty:1,
-                img:'https://www.flaticon.com/svg/static/icons/svg/531/531153.svg',
-                id:'7'
-            },
-            {
-                title:"Television",
-                price:200000,
-                qty:1,
-                img:'https://www.flaticon.com/svg/static/icons/svg/3159/3159513.svg',
-                id:'8'
-
-
-            },
-            {
-                title:"Dog",
-                price:40000,
-                qty:1,
-                img:'https://www.flaticon.com/svg/static/icons/svg/616/616554.svg',
-                id:'9'
-              }
-            ]
-       
-          }
+        product:[ ],
+        loading:true
+      }
+      this.db=firebase.firestore();
       }
 
-        
+    //   componentDidMount() {
+    //     // Reading data from firestore
+    //     firebase // from firebase 
+    //         .firestore() // database called
+    //         .collection('products') // the collection from the database
+    //         .get() // gets the instance of the collection and returns a promise
+    //         .then((snapshot) => { // promise resolves and the callback has the data from the database
+    //             let product = snapshot.docs.map((i) => { // docs contains the array of data and map function returns the array
+    //                 let o = i.data(); // here i.data() function gives the actual object containing our fields
+    //                 o['id'] = i.id; // we create another field in our object to store id
+    //                 return o; // and return this object which will be stored as an element in our product array
+    //             });
+
+    //             // then we set the state with all the objects from firestore
+    //             this.setState({
+    //                 product,
+    //                 loading: false
+    //             });
+    //         });
+    // }
+
+    componentDidMount() {
+      // Reading data from firestore
+      firebase // from firebase 
+          .firestore() // database called
+          .collection('products') // the collection from the database
+        //  .where('price','==' , 40000)
+          // .orderBy('price')  //To sort in ascending order
+           .orderBy('price','desc') // To  sort in descending order
+          .onSnapshot((snapshot) => {    //Any change in the DB will automatically refresh the browser withouth manually refreshing
+              let product = snapshot.docs.map((i) => { // docs contains the array of data and map function returns the array
+                  let o = i.data(); // here i.data() function gives the actual object containing our fields
+                  o['id'] = i.id; // we create another field in our object to store id
+                  return o; // and return this object which will be stored as an element in our product array
+              });
+
+              // then we set the state with all the objects from firestore
+              this.setState({
+                  product,
+                  loading: false
+              });
+          });
+  }
+         
 handleIncreaseQty = (x) =>{
     let index=this.state.product.indexOf(x);
-    console.log(index);
 
     let product=this.state.product;
+    
+    const docRef=firebase.firestore().collection('products').doc(product[index].id);
 
-    product[index].qty++;
-
-    this.setState({
-        product:product
+    docRef.update({
+     qty: product[index].qty+1
+    })
+    .then(()=>{
+      console.log('Updated Successfully')
+    })
+    .catch((error)=>{
+      console.log('Error',error)
     });
 }
 handleDecreaseQty = (y) =>{
     
           let index=this.state.product.indexOf(y);
-          console.log(index);
           
           let product=this.state.product;
           if(product[index].qty === 0){
               return;
           }
-          product[index].qty--;
-        
-          this.setState({
-              product:product
-          });
+         const docRef=firebase.firestore().collection('products').doc(product[index].id);
+         docRef.update({
+           qty:product[index].qty-1
+         })
+         .then(()=>{
+          console.log('Updated Successfully')
+        })
+        .catch((error)=>{
+          console.log('Error',error)
+        });
+         
     
 }
 handleDeleteProduct = (z)=>{
     let index=this.state.product.indexOf(z);
     let product=this.state.product;
-    product.splice(index,1);
-    this.setState({
-        product:product
+    const docRef=firebase.firestore().collection('products').doc(product[index].id);
+    docRef.delete()
+    .then(()=>{
+      console.log('Deleted Successfully');
+    })
+    .catch((error)=>{
+      console.log('Error',error);
     });
 }
 getProductCount=()=>{
@@ -140,10 +129,28 @@ getTotalAmount=()=>{
   return total;
 }
 
-
+// Add Product Function
+  addProduct=()=>{
+    firebase
+    .firestore()
+    .collection('products')
+    .add({
+      title:'Mobile',
+      price:35000,
+      qty:1,
+      img:'https://i.gadgets360cdn.com/products/large/moto-g9-650x800-1598251072.jpg'
+    })
+    .then((docRef)=>{
+      console.log('Product is Added', docRef)
+    })
+    .catch((error)=>{
+      console.log('Error',error)
+    });
+  }
 
 render(){
-  const {product}=this.state;
+  
+  const {product,loading}=this.state;
   return(
     <div className="App">
         <Navbar 
@@ -151,16 +158,31 @@ render(){
           total={this.getTotalAmount()}
 
         />
+            {loading && <div style={styles.loadingGif} className="loading-gif">
+           <img src="https://i.gifer.com/YCZH.gif" alt="Loading"/>
+        </div>}
+        
+       
         <Cart
           product={product}
           onIncreaseQuantity={this.handleIncreaseQty}
           onDecreaseQuantity={this.handleDecreaseQty}
           onDeleteProduct={this.handleDeleteProduct}
         />
-
+         <button onClick={this.addProduct}>Add Product</button>
     </div>
   );
 }
 }
+var styles={
+  
+  loadingGif:{
+    textAlign:'center',
+    
+  }
+}
 
 export default App;
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
+
+// generate this from firebase.google.com
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     < App />
